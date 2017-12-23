0236999bac9ae8a679a1c95f5789c68158fa772d
Javier Storni <storni@gmail.com>
2017-12-23 13:18:09 -0300

    Initial commit

---
+++ src/main/java/com/jstorni/bo/boscrapper/BoScrapperApplication.java
@@ -0,0 +1,12 @@
+package com.jstorni.bo.boscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoScrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoScrapperApplication.class, args);
+	}
+}
